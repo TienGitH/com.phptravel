3a5aaad0fff6ab797da4ab2e26dce958ce2ea94a
Tien <tienttnguyen03@gmail.com>
2021-03-28 16:18:33 +0700

    tien send request

--- src/Main/TestSuite.java
+++ src/Main/TestSuite.java
@@ -5,7 +5,6 @@ import Testcases.Inittiation;
 
 public class TestSuite {
 	WebDriver driver;
-	//public static final Logger log = Logger.getLogger(TestSuite.class.getName());
 
 	public static void main(String[] args) {
 		TestSuite testSuite = new TestSuite();
@@ -14,11 +13,6 @@ public class TestSuite {
 	}
 
 	public void Init() {
-		/*String log4jConfPath = "log4j.properties";
-		PropertyConfigurator.configure(log4jConfPath);
-
-		log.info("Tien test!");
-		log.error("Error");*/
 		Inittiation init = new Inittiation();
 		driver = init.initDriver();
 		String url = "https://www.phptravels.net/home";
